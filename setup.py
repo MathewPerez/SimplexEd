3b583e587e247597ec656e77a8b0677b7717c0ae
mathewperez <matperez755@gmail.com>
2021-01-17 18:39:22 -0700

    Add files for packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SimplexEd',
+    packages=find_packages(include=['SimplexEd']),
+    version='0.1.0',
+    description='Educational Python library to demonstrate the Simplex Algorithm',
+    author='Mathew Perez',
+    license='Apache 2.0',
+    install_requires=[],
+    setup_requires=[‘pytest-runner’],
+    tests_require=[‘pytest==4.4.1’],
+    test_suite=’tests’,
+)
